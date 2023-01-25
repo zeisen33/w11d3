7464619f3d45a38825a27d288d8d3672b204d2a9
App Academy Student <aastudent@Apps-MacBook-Air.local>
2023-01-25 15:54:43 -0500

    phase2

--- practice-for-week-14-react-context-review-with-tests-long-practice-main/src/components/SetCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice-main/src/components/SetCoffeeBean.js
@@ -1,15 +1,27 @@
+import { useContext } from "react";
+import { CoffeeContext } from "../context/CoffeeContext";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const {setCoffeeBeanId} = useContext(CoffeeContext);
+
+  const handleChange = (id) => {
+    console.log(id)
+    setCoffeeBeanId(id) 
+    
+  };
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
-        name="coffee-bean"
-      >
+        name="coffee-bean" 
+        onChange={handleChange}>
         {coffeeBeans.map(bean => (
           <option
             key={bean.id}
             value={bean.id}
-          >
+            >
+      
             {bean.name}
           </option>
         ))}
